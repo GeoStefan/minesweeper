6cb7564bb4777fdd861662795f3fe3532c269451
Popescu Georgian-Stefan <georgianspopescu@gmail.com>
2016-12-23 17:49:06 +0200

    The beginning

---
+++ Minesweeper-game/Minesweeper-game/Source.cpp
@@ -0,0 +1,14 @@
+/*
+		Minesweeper game
+		author: Popescu Georgian-Stefan
+		group: A7
+*/
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "aici implementez codul pt joc"<<endl;
+	return 0;
+}
